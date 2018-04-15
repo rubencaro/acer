c1a1d1bbc42ded380a690b10c911f9cf9691349b
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-04-15 18:24:34 +0200

    First commit

---
+++ lib/acer.ex
@@ -0,0 +1,18 @@
+defmodule Acer do
+  @moduledoc """
+  Documentation for Acer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Acer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/acer_test.exs
@@ -0,0 +1,8 @@
+defmodule AcerTest do
+  use ExUnit.Case
+  doctest Acer
+
+  test "greets the world" do
+    assert Acer.hello() == :world
+  end
+end
