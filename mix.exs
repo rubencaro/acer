5e1fd7aa09399bd7059918963554f1860beba1e0
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-04-29 20:15:49 +0200

    Add distillery

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Acer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ace, "~> 0.15.11"}
+      {:ace, "~> 0.15.11"},
+      {:distillery, "~> 1.5", runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
