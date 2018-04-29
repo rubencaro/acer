5ff77d36c0396ea661f9972083ca8bb15c14648e
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-04-29 19:47:44 +0200

    Get ace running

--- lib/acer/application.ex
+++ lib/acer/application.ex
@@ -10,11 +10,28 @@ defmodule Acer.Application do
     children = [
       # Starts a worker by calling: Acer.Worker.start_link(arg)
       # {Acer.Worker, arg},
+      {Ace.HTTP.Service, [
+          {MyApp, %{greeting: "Hello"}},
+          [port: 8080, cleartext: true]
+        ]
+      }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Acer.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+end
+
+defmodule MyApp do
+  use Raxx.Server
+
+  @impl Raxx.Server
+  def handle_request(%{method: :GET, path: []}, %{greeting: greeting}) do
+    Raxx.response(:ok)
+    |> Raxx.set_header("content-type", "text/plain")
+    |> Raxx.set_body("#{greeting}, World!")
+  end
+  def handle_request(_request, _state) do
+    Raxx.response(404)
   end
 end
--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Acer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ace, "~> 0.15.11"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
